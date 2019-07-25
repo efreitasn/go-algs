6535c4b3ccaca89e4ecaa30c43158909360dbe46
Emanuel Freitas <efreitasn@hotmail.com>
2019-07-25 20:32:18 -0300

    Fix bubblesort test file

--- bubblesort/bubblesort_test.go
+++ bubblesort/bubblesort_test.go
@@ -1,35 +1,32 @@
 package bubblesort
 
 import (
+	"fmt"
 	"reflect"
 	"testing"
 )
 
 func TestExec(t *testing.T) {
 	tests := []struct {
-		name string
-		in   []int
-		out  []int
+		in  []int
+		out []int
 	}{
 		{
-			"5, 4, 3, 2, 1",
 			[]int{5, 4, 3, 2, 1},
 			[]int{1, 2, 3, 4, 5},
 		},
 		{
-			"20, 50, 30, 10, 33",
 			[]int{20, 50, 30, 10, 33},
 			[]int{10, 20, 30, 33, 50},
 		},
 		{
-			"1, 2, 3, 50, 60",
 			[]int{1, 2, 3, 50, 60},
 			[]int{1, 2, 3, 50, 60},
 		},
 	}
 
 	for _, test := range tests {
-		t.Run(test.name, func(t *testing.T) {
+		t.Run(fmt.Sprintf("%v", test.in), func(t *testing.T) {
 			Exec(test.in)
 
 			if !reflect.DeepEqual(test.in, test.out) {
