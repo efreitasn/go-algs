1b8f02751173ac75feb0c83aed29d54bee0be65a
Emanuel Freitas <efreitasn@hotmail.com>
2022-06-28 18:13:30 -0300

    selectionsort: update to use generics

--- selectionsort/selectionsort.go
+++ selectionsort/selectionsort.go
@@ -1,8 +1,10 @@
 // Package selectionsort provides a way of sorting a slice using the selection sort algorithm.
 package selectionsort
 
-// Exec sorts a slice of ints in increasing order using the selection sort algorithm.
-func Exec(s []int) {
+import "golang.org/x/exp/constraints"
+
+// Exec sorts a slice of an ordered type T in increasing order using the selection sort algorithm.
+func Exec[T constraints.Ordered](s []T) {
 	for currentIndex := 0; currentIndex < len(s); currentIndex++ {
 		minIndex := currentIndex
 
