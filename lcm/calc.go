faaec5dd7d80b04c9072885838cc08faf577c367
Emanuel Freitas <efreitasn@hotmail.com>
2022-06-28 17:53:40 -0300

    lcm: update to use generics

--- lcm/calc.go
+++ lcm/calc.go
@@ -1,9 +1,12 @@
 // Package lcm implements algorithms related to the least common multiple.
 package lcm
 
-import "github.com/efreitasn/go-algs/gcd"
+import (
+	"github.com/efreitasn/go-algs/gcd"
+	"golang.org/x/exp/constraints"
+)
 
 // Calc calculates the least common multiple of a and b.
-func Calc(a, b int) int {
-	return a*b / gcd.Calc(a, b)
+func Calc[T constraints.Integer](a, b T) T {
+	return a * b / gcd.Calc(a, b)
 }
