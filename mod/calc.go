0b2042e9601398d07d23772130381515cbd17f7e
Emanuel Freitas <efreitasn@hotmail.com>
2022-06-28 18:08:52 -0300

    mod: update to use generics

--- mod/calc.go
+++ mod/calc.go
@@ -1,12 +1,14 @@
 // Package mod implements operations related to modular arithmetic.
 package mod
 
+import "golang.org/x/exp/constraints"
+
 // Calc calculates a mod p.
 // Note that this is not the same as the remainder of a/p.
 // The result of a mod p is a number r such that a = qp + r
 // for a number q and 0 <= r < p.
 // if p < 0, the result is 0.
-func Calc(a, p int) int {
+func Calc[T constraints.Integer](a, p T) T {
 	if p < 0 {
 		return 0
 	}
