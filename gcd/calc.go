bef656c56b1d914bea50c715e6b2ca23cc839f75
Emanuel Freitas <efreitasn@hotmail.com>
2022-09-03 22:55:12 -0300

    gcd: update to use the recursive version

--- gcd/calc.go
+++ gcd/calc.go
@@ -5,20 +5,9 @@ import "golang.org/x/exp/constraints"
 
 // Calc calculates the greatest common divisor of a and b using the Euclidean algorithm.
 func Calc[T constraints.Integer](a, b T) T {
-	var greaterInt T
-	var smallerInt T
-
-	if a > b {
-		greaterInt = a
-		smallerInt = b
-	} else {
-		greaterInt = b
-		smallerInt = a
+	if b == 0 {
+		return a
 	}
 
-	for smallerInt != 0 {
-		smallerInt, greaterInt = greaterInt%smallerInt, smallerInt
-	}
-
-	return greaterInt
+	return Calc(b, a%b)
 }
